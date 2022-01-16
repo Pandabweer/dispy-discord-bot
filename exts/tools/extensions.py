e35df5fcfda72c3ecbc36b5bd8e0e85f67268865
pandabweer <ebosss03@gmail.com>
2022-01-16 21:38:21 +0100

    Putted todo list in projects tab
    https://github.com/Pandabweer/dispy-discord-bot/projects/1?

--- exts/tools/extensions.py
+++ exts/tools/extensions.py
@@ -7,20 +7,6 @@ class Extensions(Cog, name="extension"):
     def __init__(self, bot: Dispy) -> None:
         self.bot = bot
 
-    # TODO
-    # Extension load/unload/reload PRIVATE
-    # Documents command
-    # Suggest command
-    # Database
-    # Bot managing (leave_guild, echo (Advanced), Blacklist) PRIVATE
-    # Remind command
-    # Music (Caching)
-    # Warn command
-
-    # Additional
-    # Eval command (Safe eval (SnekBox))
-    # Website
-
     @command(name="reload", aliases=["r"])
     @is_owner()
     async def extension_reload(self, ctx: Context, *, ext: str = None):
